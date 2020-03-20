3e1e85e5fab5669b3869c10eeaf6f4814f54ca1a
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-03-20 17:24:13 -0600

    Se creo el objeto "herramientasMinecraft" con sus respectivos atrivos, get y set y su constructor !!

---
+++ Laboratorio 2/Tarea/HerramientasMinecraft/src/com/company/herramientasMinecraft.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class herramientasMinecraft {
+    private String color, tamaño, duenio;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
+
+    public int getNumFilo() {
+        return numFilo;
+    }
+
+    public void setNumFilo(int numFilo) {
+        this.numFilo = numFilo;
+    }
+
+    public int getNumDaño() {
+        return numDaño;
+    }
+
+    public void setNumDaño(int numDaño) {
+        this.numDaño = numDaño;
+    }
+
+    private int numFilo, numDaño;
+
+    public herramientasMinecraft(String color, String tamaño, String duenio, int numFilo, int numDaño) {
+        this.color = color;
+        this.tamaño = tamaño;
+        this.duenio = duenio;
+        this.numFilo = numFilo;
+        this.numDaño = numDaño;
+    }
+}
